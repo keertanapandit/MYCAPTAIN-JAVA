8e2d144180c82b65c19e2f6ee2f3160b81962138
keertanapandit <75423482+keertanapandit@users.noreply.github.com>
2021-06-23 17:49:51 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args) 
+    {
+       
+        Scanner sc=new Scanner(System.in);
+        int t1 = 0, t2 = 1;
+        System.out.print("Enter the number of terms: ");
+        int n=sc.nextInt();   //Declare and Initialize the number of terms
+        System.out.println("First " + n + " terms of fibonnaci series: ");
+        //Print the fibonacci series
+        int i = 1; 
+        while (i <= n)
+        {
+            System.out.print(t1 + " ");
+            int sum = t1 + t2;
+            t1 = t2;
+            t2 = sum;
+            i++;
+        }
+    }
+}
